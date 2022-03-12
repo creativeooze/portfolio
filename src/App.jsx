0633dfb8a94e35cd6603377d434e83925e55981f
creativeooze <anshuman.nanda.1999@gmail.com>
2022-03-12 15:33:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./app.scss";
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
+import { useState } from "react";
+
+function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <div className='app'>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}></Topbar>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}></Menu>
+      <div className="sections">
+        <Intro></Intro>
+        <Portfolio></Portfolio>
+        <Works></Works>
+        <Testimonials></Testimonials>
+        <Contact></Contact>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,42 @@
+import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
+
+
+export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            backDelay: 1500,
+            backSpeed: 60,
+            showCursor: true,
+            strings: [
+                'Salesforce Developer',
+                'React Js Developer',
+                'Java Developer',
+                'Python Developer'
+            ]
+        })
+    }, [])
+    return (
+        <div className='intro' id="intro">
+            <div className='left'>
+                <div className='imgContainer'>
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className='right'>
+                <div className='wrapper'>
+                    <h2>Hi there, I am</h2>
+                    <h1>Anshuman Nanda</h1>
+                    <h3>I am a <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
+        </div>
+    )
+}
